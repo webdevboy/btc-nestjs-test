53937e4f0db46d2ab549d6cb6383efcbf014dddb
webdevboy <ilhyok89@gmail.com>
2021-04-12 16:31:01 +0300

    add test for getPrice function

--- server/src/price/price.service.spec.ts
+++ server/src/price/price.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { IPrice } from './interfaces/price';
+import { PriceModule } from './price.module';
 import { PriceService } from './price.service';
 
 describe('PriceService', () => {
@@ -7,6 +10,7 @@ describe('PriceService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PriceService],
+      imports: [HttpModule],
     }).compile();
 
     service = module.get<PriceService>(PriceService);
@@ -15,4 +19,13 @@ describe('PriceService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return price object', async () => {
+    const result = {
+      assetId: 'BTC',
+      value: '123.2',
+    };
+    jest.spyOn(service, 'getPrice').mockImplementation(() => new Promise((resolve, reject) => {resolve(result)}));
+    expect(await service.getPrice('BTC')).toEqual(result);
+  })
 });
